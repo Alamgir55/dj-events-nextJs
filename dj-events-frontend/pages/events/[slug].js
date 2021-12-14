c0050d33fbde2711a00c35583885c04b091bc2ae
alamgir hossain <alamgir555hossain@gmail.com>
2021-12-13 22:19:55 -0500

    slug, serverside-rendering

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -1,11 +1,65 @@
-import { useRouter } from "next/router";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/Event.module.css";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventPage() {
-  const router = useRouter();
-
+export default function EventPage({ evt }) {
+  const deleteEvent = (e) => {
+    console.log("delete");
+  };
   return (
-    <div>
-      <h1>My Event Page</h1>
-    </div>
+    <Layout>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+      </div>
+    </Layout>
   );
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// }
