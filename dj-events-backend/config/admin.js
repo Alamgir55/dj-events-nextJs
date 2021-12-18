00596ea29fd2ecf15020d86258e4a16a8ee6269b
alamgir hossain <alamgir555hossain@gmail.com>
2021-12-17 20:12:57 -0500

    bug-fixed

--- dj-events-backend/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '486d5769d21772c56d9423558d60a728'),
-  },
-});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c49706ab-4432-4df8-9ed3-a1676476c7e6'
+};
