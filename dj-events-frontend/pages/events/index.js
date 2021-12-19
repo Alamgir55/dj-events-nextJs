d3291cca2833630710bdf0593c88163eef760acc
alamgir hossain <alamgir555hossain@gmail.com>
2021-12-18 21:21:58 -0500

    add-form0submit/Search0page

--- dj-events-frontend/pages/events/index.js
+++ dj-events-frontend/pages/events/index.js
@@ -2,7 +2,7 @@ import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import EventItem from "@/components/EventItem";
 
-export default function EventsPag({ events }) {
+export default function EventsPage({ events }) {
   return (
     <Layout>
       <h1>Events</h1>
@@ -16,7 +16,7 @@ export default function EventsPag({ events }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
   const events = await res.json();
   //console.log(events);
 
